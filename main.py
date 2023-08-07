addf97f17d06f2030ddd5cff89b0da336c5cc073
Amit Roy <amit.akr.roy@gmail.com>
2023-08-07 17:27:00 +0530

    Pep 8 standard

--- main.py
+++ main.py
@@ -1,66 +1,75 @@
-from flask import Flask, Response, jsonify, request
 import requests
 import xml.etree.ElementTree as ET
+from flask import Flask, request, jsonify, Response
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
 
-api_key='' # Your API-Key
-url="https://weatherapi-com.p.rapidapi.com/current.json"
+api_key = ''  # Your API-Key
+url = "https://weatherapi-com.p.rapidapi.com/current.json"
 
 headers = {
-    "X-RapidAPI-Key" : api_key,
+    "X-RapidAPI-Key": api_key,
     "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com"
 }
 
-@app.route("/getCurrentWeather",methods=['POST'])
+
+def get_xml_output(output):
+    root = ET.Element('root')
+    for key, value in output.items():
+        elem = ET.SubElement(root, key if key != "Weather" else "Temperature")
+        elem.text = str(value.split()[0]) if key == "City" else str(value)
+    xml_declaration = '<?xml version="1.0" encoding="UTF-8" ?>\n'
+    xml_output = (
+        xml_declaration + ET.tostring(root, encoding='utf-8').decode('utf-8')
+    )
+
+    return xml_output
+
+
+@app.route("/getCurrentWeather", methods=['POST'])
 def get_current_weather():
     try:
-        input = request.json
-        #print(request.json)
-        if 'city' in input and 'output_format' in input :
-            city = input['city']
-            output_format = input['output_format']
-        else :
-            return jsonify({'error':'Invalid JSON data. Missing Parameters'}),400 
-        
-        querystring = {"q":city}
+        input_data = request.json
+
+        if 'city' in input_data and 'output_format' in input_data:
+            city = input_data['city']
+            output_format = input_data['output_format']
+        else:
+            return (
+                jsonify({'error': 'Invalid JSON data. Missing Parameters'}),
+                400
+            )
+
+        querystring = {"q": city}
         output = {}
-        response = requests.get(url,headers=headers,params= querystring)
+        response = requests.get(url, headers=headers, params=querystring)
         response_json = response.json()
-        latitude = response_json.get('location').get('lat')
-        longitude = response_json.get('location').get('lon')
-        country = response_json.get('location').get('country')
-        weather = response_json.get('current').get('temp_c')
-        output["Weather"] = str(weather)+" C"
+        location = response_json.get('location')
+        current = response_json.get('current')
+
+        latitude = location.get('lat')
+        longitude = location.get('lon')
+        country = location.get('country')
+        weather = current.get('temp_c')
+
+        output["Weather"] = str(weather) + " C"
         output["Latitude"] = latitude
         output["Longitude"] = longitude
         output["City"] = city.capitalize() + " " + country
+
         if output_format == 'json':
             return jsonify(output)
         elif output_format == 'xml':
-            root = ET.Element('root')
-            for key, value in output.items():
-                if key == "Weather":
-                    elem = ET.SubElement(root,"Temperature")
-                else :
-                    elem = ET.SubElement(root, key)
-                if key == "City":
-                    elem.text= str(value.split()[0])
-                else :
-                    elem.text = str(value)
-            xml_declaration = '<?xml version="1.0" encoding="UTF-8" ?>\n'
-            xml_output = xml_declaration + ET.tostring(root,encoding='utf-8').decode('utf-8')
+            xml_output = get_xml_output(output)
             return Response(xml_output, content_type='application/xml')
-        else :
-            return jsonify({'error':'Invalid Output Format'}),500
-        
+        else:
+            return jsonify({'error': 'Invalid Output Format'}), 500
+
     except Exception as e:
-         print(e)
-         return jsonify({'error':'Invalid JSON data'}),500
+        print(e)
+        return jsonify({'error': 'Invalid JSON data'}), 500
+
 
 if __name__ == '__main__':
-	app.run(debug=True,host='0.0.0.0',port=5432)
+    app.run(host='0.0.0.0', port=5432)
